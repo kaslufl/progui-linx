6c3de77b69dd7aed48c48b5f1a6d8ae7c5267cac
kaslufl <leallucasfonseca@gmail.com>
2020-12-22 21:52:39 -0300

    feat: adiciona a lista json no controller

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,8 +1,11 @@
 from application import app
 from flask import render_template, request
-from application.model.entity.product import Product
+from application.model.dao.product_dao import Product_DAO
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    product_dao = Product_DAO
+    product_dao.get_products_json()
+    product_list = product_dao.get_product_list()
+    return render_template("home.html", product_list = product_list)
