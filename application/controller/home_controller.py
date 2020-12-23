12679a298bf342e42b690f30cb82041774673036
kaslufl <leallucasfonseca@gmail.com>
2020-12-22 21:57:59 -0300

    fix: corrige erros de sintaxe que não permitiam o funcionamento do flask

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -5,7 +5,7 @@ from application.model.dao.product_dao import Product_DAO
 @app.route("/")
 @app.route("/home")
 def home():
-    product_dao = Product_DAO
+    product_dao = Product_DAO()
     product_dao.get_products_json()
     product_list = product_dao.get_product_list()
     return render_template("home.html", product_list = product_list)
--- application/model/dao/product_dao.py
+++ application/model/dao/product_dao.py
@@ -7,8 +7,8 @@ class Product_DAO:
         self.product_list = []
 
     def get_products_json(self):
-        with open('C:\Users\Lucas Leal\Desktop\progui-linx\products.json') as product_file
-        self.json_list = json.load(product_file)
+        with open('C:\\Users\\Lucas Leal\\Desktop\\progui-linx\\products.json') as product_file:
+            self.json_list = json.load(product_file)
         for product in self.json_list:
             self.product_list.append(Product(product['id'], product['name'], product['image'], product['oldPrice'], product['price'], product['description'], product['installments']['count'], product['installments']['value']))
 
